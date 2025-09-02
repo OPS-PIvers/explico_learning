fde3a680ffe0242784bd0e51be16f286e2161c18
Paul Ivers <138139858+OPS-PIvers@users.noreply.github.com>
2025-09-02 20:22:24 +0000

    broken project loading

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Clear the dist directory
+rm -rf dist/*
+
+# Copy the necessary files to the dist directory
+cp Code.gs dist/
+cp constants.gs dist/
+cp *.html dist/
+cp appsscript.json dist/
