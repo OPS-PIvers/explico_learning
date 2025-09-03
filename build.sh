73fbf3916803713459d073e92082e50b2623f7f0
Paul Ivers <138139858+OPS-PIvers@users.noreply.github.com>
2025-09-03 19:36:55 +0000

    fix: Refactor template includes to fix loading errors

--- build.sh
+++ build.sh
@@ -11,7 +11,9 @@ done
 
 cp templates/*.html dist/
 cp appsscript.json dist/
-cp styles.css dist/
+echo "<style>" > dist/styles.html
+cat styles.css >> dist/styles.html
+echo "</style>" >> dist/styles.html
 cp utils/styles.css dist/
 cp Code.gs dist/
 cp constants.gs dist/
